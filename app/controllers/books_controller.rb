d2d20475aed41b16a8f0b27fabb0d20c935ac7de
EC2 Default User <133302303+matsuno24@users.noreply.github.com>
2023-06-18 09:49:06 +0000

    [Update]Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,10 +9,19 @@ class BooksController < ApplicationController
      @book = Book.new(book_params)
      @book.user_id = current_user.id
      @book.save
-     redirect_to book_path(book.id)
+     redirect_to book_path(@book.id)
   end
 
   def show
+    @books = Book.find(params[:id])
+    @user = current_user
+    @book = Book.new
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
+    @user = current_user
   end
 
   def show
+    @book = Book.new
+    @user = current_user
   end
 
   def edit
