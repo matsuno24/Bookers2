409a3a891a3c5d0753fe1bbe1f0f4aff3e7a32a8
EC2 Default User <133302303+matsuno24@users.noreply.github.com>
2023-06-25 07:35:38 +0000

    [Finish]Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:image, :name, :introduction)
+    params.require(:user).permit(:profile_image, :name, :introduction)
   end
   
   
